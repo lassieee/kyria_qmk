6b445884c909e833e46652f7df8d9a7ff1528e89
Casper Lasschuit <c.c.lasschuit@hva.nl>
2022-12-09 17:20:48 +0100

    fix not able to go back to base layer

--- lassieee/keycodes.h
+++ lassieee/keycodes.h
@@ -11,10 +11,11 @@ enum layers {
     _L5,
 };
 
-#define L0  DF(_L0)
-#define L1  TO(_L1)
-#define L2  TO(_L2)
-#define L3  TO(_L3)
-#define L4  TO(_L4)
-#define L5  TO(_L5)
+#define QWERTY DF(_L0)
+#define L0 TO(_L0)
+#define L1 TO(_L1)
+#define L2 TO(_L2)
+#define L3 TO(_L3)
+#define L4 TO(_L4)
+#define L5 TO(_L5)
 
